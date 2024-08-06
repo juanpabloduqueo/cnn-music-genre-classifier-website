b20ea112f11e9e7084ada5b4b7c46fdf10314ed0
Juan Pablo Duque <juanpabloduqueo@gmail.com>
2024-08-05 22:23:07 -0500

    prepare files for pythonanywhere deployment1

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,13 @@
-from app import app
+import sys
+import os
 
-if __name__ == "__main__":
-    app.run()
+# Add your project directory to the sys.path
+project_home = '/home/juanduqueo/music-genre-classifier'
+if project_home not in sys.path:
+    sys.path = [project_home] + sys.path
+
+# Set the Flask app's environment variable
+os.environ['FLASK_APP'] = 'app.py'
+
+# Import the Flask app
+from app import app as application
